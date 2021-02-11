87f0b1599ad87ef2292c1923eb9ba96ee7ecd5b4
nbulygin <n.bulygin@city-mobil.ru>
2021-02-11 15:45:59 +0300

    Initial commit

---
+++ app/src/androidTest/java/com/example/drawablebug/ExampleInstrumentedTest.kt
@@ -0,0 +1,33 @@
+package com.example.drawablebug
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import com.agoda.kakao.image.KImageView
+import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+
+    @get:Rule
+    var rule: ActivityScenarioRule<MainActivity> = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun myTest() {
+        val scenario = rule.scenario
+        KImageView {
+            withId(R.id.imageViewBottomTabItem)
+            withDrawable(resId = R.drawable.test_drawable)
+        }.isDisabled()
+    }
+
+}
---
+++ app/src/main/java/com/example/drawablebug/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.drawablebug
+
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById<ImageView>(R.id.imageViewBottomTabItem).setImageResource(R.drawable.test_drawable)
+    }
+}
